f03779c33fe7b8ef4fdc5aba886f9065e98cbb97
hindustantech <kumarrupees505@gmail.com>
2026-04-08 14:51:05 +0530

    add

--- routes/permissionRoutes.js
+++ routes/permissionRoutes.js
@@ -28,6 +28,6 @@ router.delete('/:id', authMiddleware, checkPermission('permission.delete'), dele
 router.post('/assign', authMiddleware, checkPermission('permission.assign'), assignPermission);
 router.post('/remove', authMiddleware, checkPermission('permission.assign'), removePermission);
 // Any admin can view
-router.get('/:userId', authMiddleware, getUserPermissions);
+router.get('/', authMiddleware, getUserPermissions);
 
 export default router;
