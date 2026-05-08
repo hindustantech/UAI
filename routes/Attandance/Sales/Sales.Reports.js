bd5bdcdc6008e1bd09f960ac393895b4f185a31b
hindustantech <kumarrupees505@gmail.com>
2026-05-08 10:27:00 +0530

    add

--- routes/Attandance/Sales/Sales.Reports.js
+++ routes/Attandance/Sales/Sales.Reports.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { exportSalesReport } from "../../../controllers/attandance/Sales/SalesReoptExport.js";
+import { exportSalesReport ,exportSalesPersonReport} from "../../../controllers/attandance/Sales/SalesReoptExport.js";
 import authMiddleware from "../../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/export", authMiddleware, exportSalesReport);
+router.get("/exportSalesPersonReport", authMiddleware, exportSalesPersonReport);
 
 
 export default router;
