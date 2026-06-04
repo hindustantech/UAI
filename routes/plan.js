912f670b838c85bf2e8b924d5e9804c1b784c547
hindustantech <kumarrupees505@gmail.com>
2026-06-04 15:22:15 +0530

    Plan auto check out

--- routes/plan.js
+++ routes/plan.js
@@ -9,16 +9,17 @@ import {
     getPlanById,
     toggleAutoCheckout
 } from '../controllers/attandance/Subscriptions/Plan.js';
-import { checkPermission } from '../middlewares/checkPermission.js';
+import { checkPermission } from '../middlewares/checkPermission.js'
+import authMiddleware from '../middlewares/authMiddleware.js';
 const router = express.Router();
-
+    all
 // Plan Routes
-router.post('/', checkPermission('plans.create'), createPlan);                    // Create Plan
+router.post('/', authMiddleware, checkPermission('plans.create'), createPlan);                    // Create Plan
 router.get('/', getAllPlans);                    // Get All Plans (with pagination & filters)
 router.get('/:id', getPlanById);                 // Get Single Plan
-router.put('/:id', checkPermission('plans.update'), updatePlan);                  // Update Plan
-router.delete('/:id', checkPermission('plans.delete'), deletePlan);               // Delete Plan
-router.patch('/toggle/:id', checkPermission('plans.update'), togglePlanStatus);   // Toggle Active/Inactive
+router.put('/:id', authMiddleware, checkPermission('plans.update'), updatePlan);                  // Update Plan
+router.delete('/:id', authMiddleware, checkPermission('plans.delete'), deletePlan);               // Delete Plan
+router.patch('/toggle/:id', authMiddleware, checkPermission('plans.update'), togglePlanStatus);   // Toggle Active/Inactive
 // Toggle Active/Inactive
-router.patch('/auto-checkout/:planId', checkPermission('plans.update'), toggleAutoCheckout); // Toggle Auto Check-Out
+router.patch('/auto-checkout/:planId', authMiddleware, checkPermission('plans.update'), toggleAutoCheckout); // Toggle Auto Check-Out
 export default router;
