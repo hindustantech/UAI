7539374a1d989ea0bd2bb2c13c0719bac7fd83e9
hindustantech <kumarrupees505@gmail.com>
2026-07-02 15:51:05 +0530

    add

--- controllers/onboarding.controller.js
+++ controllers/onboarding.controller.js
@@ -273,10 +273,21 @@ export const createLead = async (req, res) => {
                 <td>${lead.notes || "N/A"}</td>
             </tr>
 
-            <tr>
-                <td><strong>Created At</strong></td>
-                <td>${new Date().toLocaleString()}</td>
-            </tr>
+           <tr>
+    <td><strong>Created At</strong></td>
+    <td>
+        ${new Date().toLocaleString("en-IN", {
+                timeZone: "Asia/Kolkata",
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: true, // false for 24-hour format
+            })}
+    </td>
+</tr>
         </table>
 
         <br>
